6aab4d828cc2184e97b56dd2d52279fffd06c99c
blurred <jasonseril@gmail.com>
2015-10-18 00:17:50 +0900

    added some functions

--- simple-todos.js
+++ simple-todos.js
@@ -39,4 +39,15 @@ Template.body.events({
   }
 });
 
+  Template.task.events ({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
