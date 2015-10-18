874cd1ab7899fd037d5ac3de372e5f8e38a50594
blurred <jasonseril@gmail.com>
2015-10-18 16:44:54 +0900

    added some functions

--- simple-todos.js
+++ simple-todos.js
@@ -18,7 +18,27 @@ if (Meteor.isClient){
     tasks: function () {
       return Tasks.find({});
     }
+
   });
+
+  Template.body.helpers({
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
+
 
 Template.body.events({
   "submit .new-task": function (event) {
@@ -36,6 +56,9 @@ Template.body.events({
 
     // Clear form
     event.target.text.value = "";
+  },
+  "change .hide-completed input": function (event) {
+    Session.set("hideCompleted", event.target.checked);
   }
 });
 
